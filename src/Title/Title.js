d95644155e0853cd91b21df39ae45474bc50ad46
Phil DeLong <philjdelong@gmail.com>
2020-11-12 13:09:59 -0700

    Create title component

---
+++ src/Title/Title.js
@@ -0,0 +1,11 @@
+import '../Title/Title.css'
+
+const Title = (props) => {
+    return(
+        <div>
+            <h1>Welcome to the <code className="title">BLOGG</code></h1>
+        </div>
+    )
+}
+
+export default Title;
