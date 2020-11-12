d74315d48a08775911b0d833283a5ba3b034e0e0
Phil DeLong <philjdelong@gmail.com>
2020-11-12 13:09:36 -0700

    Add User, Title, and Button components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react'
 import './App.css';
 import User from './User/User.js'
+import Title from './Title/Title.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hi, I'm <code className="p-text">Phil</code> and I like React.
-        </p>
-      <User name="Capo DeLong" age="7" feeling="Love">I really like turtles.</User>
-      <User name="Brick Tomlinson" age="61" feeling="Throughly Enjoy">I love lamp.</User>
-      </header>
-    </div>
-  );
+// can only set state using this type of "Class-based component"
+class App extends Component {
+  state = {
+    users: [
+      {name: "Phil DeLong", age: 30, feeling: "Love"},
+      {name: "Capo DeLong", age: 7, feeling: "Thoroughly enjoy"},
+      {name: "Brick Tomlinson", age: 61, feeling: "Want tomato."}
+    ]
+  };
+
+  switchUserHandler = () => {
+    console.log("Clicked!")
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Title />
+          <button onClick={this.switchUserHandler}>Switch User</button>
+          <User name={this.state.users[0].name} age={this.state.users[0].age} feeling={this.state.users[0].feeling}></User>
+          <User name={this.state.users[1].name} age={this.state.users[1].age} feeling={this.state.users[1].feeling}></User>
+          <User name={this.state.users[2].name} age={this.state.users[2].age} feeling={this.state.users[2].feeling}>I love lamp.</User>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
