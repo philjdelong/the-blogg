005f313012e42c424ff681ad7ac289205c438af0
Phil DeLong <philjdelong@gmail.com>
2020-11-12 17:22:14 -0700

    Update to follow blog naming convention

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './App.css';
-import User from './User/User.js'
-import Title from './Title/Title.js'
+import Post from './Post/Post.js';
+import Title from './Title/Title.js';
 
-// can only set state using this type of "Class-based component"
 class App extends Component {
   state = {
-    users: [
-      {name: "Phil DeLong", age: 30, feeling: "Love"},
-      {name: "Capo DeLong", age: 7, feeling: "Thoroughly enjoy"},
-      {name: "Brick Tomlinson", age: 61, feeling: "Want tomato."}
+    posts: [
+      {title: "First Post", tag: "general"},
+      {title: "Capo and Sampson", tag: "about"},
+      {title: "Hobbies", tag: "about"},
     ]
   };
-
-  switchUserHandler = () => {
-    console.log("Clicked!")
-  }
-
+  
+  postChanger = () => {
+    this.setState({     
+      posts: [
+        {title: "New Title", tag: "general"},
+        {title: "Teddles", tag: "RIP"},
+        {title: "Hobbies", tag: "about"},
+      ]
+    });
+    console.log("Click happens!");
+  };
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <Title />
-          <button onClick={this.switchUserHandler}>Switch User</button>
-          <User name={this.state.users[0].name} age={this.state.users[0].age} feeling={this.state.users[0].feeling}></User>
-          <User name={this.state.users[1].name} age={this.state.users[1].age} feeling={this.state.users[1].feeling}></User>
-          <User name={this.state.users[2].name} age={this.state.users[2].age} feeling={this.state.users[2].feeling}>I love lamp.</User>
+          <button onClick={this.postChanger}>Update Post</button>
+          <Post title={this.state.posts[0].title} tag={this.state.posts[0].tag}>Here's my first post on the BLOGG</Post>
+          <Post title={this.state.posts[1].title} tag={this.state.posts[1].tag}>I love my animals!</Post>
+          <Post title={this.state.posts[2].title} tag={this.state.posts[2].tag}>I enjoy Coding, Hiking, and Spending time with the family.</Post>
         </header>
       </div>
     );
-  }
-}
+  };
+};
 
 export default App;
